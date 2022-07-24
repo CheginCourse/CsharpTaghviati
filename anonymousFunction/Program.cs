00bfdf03791c859cf92438a3cfd002ef622c4143
flutclass <flutclassgr1@gmail.com>
2022-07-24 23:01:25 +0430

    anonymouseFunction

---
+++ anonymousFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        delegate int del (int a);
+          static  del delIGetName;
+        static void Main(string[] args)
+        {
+            delIGetName =(x)=>{
+                return x* x;
+            };
+           System.Console.WriteLine(delIGetName(29)); 
+            Func<int, int, int> Kfuc = (x, y) =>
+            {
+                System.Console.WriteLine("asd");
+                return x + 9;
+            };
+
+          System.Console.WriteLine(Kfuc(1,4));  
+        }
+    }
+}
